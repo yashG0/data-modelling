494e9f275f49bc443520a8d8ea43f9a3fd7c5b12
yashG0 <89720212+yashG0@users.noreply.github.com>
2024-01-01 18:25:43 +0530

    Initial commit

---
+++ models/ecommerce/product.models.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    productImage: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    stock: {
+      default: 0,
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.Model('Product', productSchema);
